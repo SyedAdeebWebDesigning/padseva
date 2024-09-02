06fb87991891ef76d966a7966ef9279c1b904d63
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-09-03 00:35:21 +0530

    Update layout and add Padseva icon

--- components/shared/HeroSection.tsx
+++ components/shared/HeroSection.tsx
@@ -10,20 +10,20 @@ const HeroSection: React.FC = () => {
 	const { scrollY } = useScroll();
 
 	// Adjust transform ranges for enhanced parallax effect towards corners
-	const leaf1X = useTransform(scrollY, [0, 1000], [0, -400]);
-	const leaf1Y = useTransform(scrollY, [0, 1000], [0, -400]);
-	const leaf2X = useTransform(scrollY, [0, 1000], [0, 400]);
-	const leaf2Y = useTransform(scrollY, [0, 1000], [0, -400]);
-	const leaf3X = useTransform(scrollY, [0, 1000], [0, -400]);
-	const leaf3Y = useTransform(scrollY, [0, 1000], [0, 400]);
-	const leaf4X = useTransform(scrollY, [0, 1000], [0, 400]);
-	const leaf4Y = useTransform(scrollY, [0, 1000], [0, 400]);
+	const leaf1X = useTransform(scrollY, [0, 1000], [0, -200]);
+	const leaf1Y = useTransform(scrollY, [0, 1000], [0, -200]);
+	const leaf2X = useTransform(scrollY, [0, 1000], [0, 200]);
+	const leaf2Y = useTransform(scrollY, [0, 1000], [0, -200]);
+	const leaf3X = useTransform(scrollY, [0, 1000], [0, -200]);
+	const leaf3Y = useTransform(scrollY, [0, 1000], [0, 200]);
+	const leaf4X = useTransform(scrollY, [0, 1000], [0, 200]);
+	const leaf4Y = useTransform(scrollY, [0, 1000], [0, 200]);
 
 	return (
 		<div className="relative h-screen bg-[#F7E7EF] overflow-hidden flex items-center justify-center">
 			{/* Background text/logo */}
 			<div className="absolute text-center z-20">
-				<h1 className="text-[#91373E] font-bold oswald-300 text-9xl">
+				<h1 className="text-[#91373E] font-bold oswald-300 text-6xl sm:text-7xl md:text-8xl lg:text-9xl">
 					Padseva
 				</h1>
 			</div>
@@ -37,7 +37,7 @@ const HeroSection: React.FC = () => {
 					alt="Flower Top Left"
 					width={600}
 					height={600}
-					className="w-[800px] h-auto"
+					className="w-[200px] sm:w-[300px] md:w-[350px] lg:w-[600px] h-auto"
 				/>
 			</motion.div>
 
@@ -49,7 +49,7 @@ const HeroSection: React.FC = () => {
 					alt="Flower Top Right"
 					width={600}
 					height={600}
-					className="w-[600px] h-auto"
+					className="w-[200px] sm:w-[300px] md:w-[350px] lg:w-[600px] h-auto"
 				/>
 			</motion.div>
 
@@ -61,7 +61,7 @@ const HeroSection: React.FC = () => {
 					alt="Flower Bottom Left"
 					width={600}
 					height={600}
-					className="w-[600px] h-auto"
+					className="w-[200px] sm:w-[300px] md:w-[350px] lg:w-[600px] h-auto"
 				/>
 			</motion.div>
 
@@ -73,7 +73,7 @@ const HeroSection: React.FC = () => {
 					alt="Flower Bottom Right"
 					width={600}
 					height={600}
-					className="w-[800px] h-auto"
+					className="w-[200px] sm:w-[300px] md:w-[350px] lg:w-[600px] h-auto"
 				/>
 			</motion.div>
 		</div>
