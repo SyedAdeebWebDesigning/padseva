dc80e4b5cbfd98778337193362ef13bf4c88a936
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-09-03 00:20:55 +0530

    Add new files and make code changes

---
+++ components/shared/HeroSection.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import React from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import flowerImage from "/public/flowers.webp"; // Ensure correct path to the image
+import leaf from "/public/leaf.webp"; // Ensure correct path to the image
+
+const HeroSection: React.FC = () => {
+	const { scrollY } = useScroll();
+
+	// Adjust transform ranges for enhanced parallax effect towards corners
+	const leaf1X = useTransform(scrollY, [0, 1000], [0, -400]);
+	const leaf1Y = useTransform(scrollY, [0, 1000], [0, -400]);
+	const leaf2X = useTransform(scrollY, [0, 1000], [0, 400]);
+	const leaf2Y = useTransform(scrollY, [0, 1000], [0, -400]);
+	const leaf3X = useTransform(scrollY, [0, 1000], [0, -400]);
+	const leaf3Y = useTransform(scrollY, [0, 1000], [0, 400]);
+	const leaf4X = useTransform(scrollY, [0, 1000], [0, 400]);
+	const leaf4Y = useTransform(scrollY, [0, 1000], [0, 400]);
+
+	return (
+		<div className="relative h-screen bg-[#F7E7EF] overflow-hidden flex items-center justify-center">
+			{/* Background text/logo */}
+			<div className="absolute text-center z-20">
+				<h1 className="text-[#91373E] font-bold oswald-300 text-9xl">
+					Padseva
+				</h1>
+			</div>
+
+			{/* Moving flower images with fixed positioning */}
+			<motion.div
+				className="fixed z-10"
+				style={{ top: "0", left: "0", x: leaf1X, y: leaf1Y }}>
+				<Image
+					src={flowerImage}
+					alt="Flower Top Left"
+					width={600}
+					height={600}
+					className="w-[800px] h-auto"
+				/>
+			</motion.div>
+
+			<motion.div
+				className="fixed z-10"
+				style={{ top: "0", right: "0", x: leaf2X, y: leaf2Y }}>
+				<Image
+					src={leaf}
+					alt="Flower Top Right"
+					width={600}
+					height={600}
+					className="w-[600px] h-auto"
+				/>
+			</motion.div>
+
+			<motion.div
+				className="fixed z-10"
+				style={{ bottom: "0", left: "0", x: leaf3X, y: leaf3Y }}>
+				<Image
+					src={leaf}
+					alt="Flower Bottom Left"
+					width={600}
+					height={600}
+					className="w-[600px] h-auto"
+				/>
+			</motion.div>
+
+			<motion.div
+				className="fixed z-10"
+				style={{ bottom: "0", right: "0", x: leaf4X, y: leaf4Y }}>
+				<Image
+					src={flowerImage}
+					alt="Flower Bottom Right"
+					width={600}
+					height={600}
+					className="w-[800px] h-auto"
+				/>
+			</motion.div>
+		</div>
+	);
+};
+
+export default HeroSection;
---
+++ components/shared/Section.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+interface SectionProps {}
+
+const Section = ({}: SectionProps) => {
+	return (
+		<section className="h-[80vh] flex items-center justify-center">
+			<div>1</div>
+			<div>2</div>
+		</section>
+	);
+};
+
+export default Section;
