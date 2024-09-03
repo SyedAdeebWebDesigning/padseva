3f12413b3c9eb240814f713ffdd55f531b611590
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-09-03 19:37:45 +0530

    Add Image component to HeroSection

--- components/shared/HeroSection.tsx
+++ components/shared/HeroSection.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
 
 const HeroSection: React.FC = () => {
@@ -7,9 +8,9 @@ const HeroSection: React.FC = () => {
 		<div className="sticky -z-10 top-0 w-full h-screen bg-[#F7E7EF] overflow-hidden flex items-center justify-center">
 			{/* Background text/logo */}
 			<div className="absolute text-center z-20">
-				<h1 className="text-[#91373E] font-bold oswald-300 text-6xl sm:text-7xl md:text-8xl lg:text-9xl">
-					Padseva
-				</h1>
+				<div className="relative lg:size-[700px] size-[400px] z-[40]">
+					<Image src={"/padseva-logo-2.png"} fill alt="logo" />
+				</div>
 			</div>
 
 			{/* Moving flower images with high z-index */}
