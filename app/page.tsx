1f07734cf8ccb9a4afcb11b2651681306274d89d
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-09-18 22:56:15 +0530

    Update user authentication logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Animation from "@/components/shared/Animation";
 import HeroSection from "@/components/shared/HeroSection";
 import Journey from "@/components/shared/Journey";
-import MajorSections from "@/components/shared/MajorSections";
+import Missions from "@/components/shared/Missions";
+import OurFounder from "@/components/shared/OurFounder";
 import PlaceHolderImage from "@/components/shared/PlaceHolderImage";
 import Section from "@/components/shared/Section";
 import Volunteers from "@/components/shared/Volunteers";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
 	return (
@@ -24,9 +27,35 @@ export default function Home() {
 					<PlaceHolderImage />
 				</section>
 
-				<section className="relative">
-					<MajorSections />
+				<section className="w-full bg-[#ffe8e8] relative ">
+					<Missions />
 				</section>
+				<section className="sticky bg-white xl:w-[50%] w-full z-[999] top-10 py-4">
+					<nav className="flex flex-wrap items-center justify-end w-full">
+						<ul className="flex justify-around items-center xl:w-[50%] w-full">
+							<Link
+								href={"#founder"}
+								className={buttonVariants({ variant: "link" })}>
+								Founder
+							</Link>
+							<Link
+								href={"#team"}
+								className={buttonVariants({ variant: "link" })}>
+								Team
+							</Link>
+							<Link href={"#"} className={buttonVariants({ variant: "link" })}>
+								Link 3
+							</Link>
+							<Link href={"#"} className={buttonVariants({ variant: "link" })}>
+								Link 4
+							</Link>
+						</ul>
+					</nav>
+				</section>
+				<section className="w-full bg-[#ffe8e8] relative -mt-20">
+					<OurFounder />
+				</section>
+
 				<section className="bg-black/50 relative z-[900]  backdrop-blur-xl">
 					<Journey />
 				</section>
