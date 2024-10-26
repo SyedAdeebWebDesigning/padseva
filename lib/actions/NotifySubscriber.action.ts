3003fd5975aba28996a8e69561849a2eed183796
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-10-27 00:25:01 +0530

    Update generateEmailContent function

--- lib/actions/NotifySubscriber.action.ts
+++ lib/actions/NotifySubscriber.action.ts
@@ -42,14 +42,11 @@ async function sendEmailsInBatches(
 function generateEmailContent(issue: any): string {
 	return `
     <h1>New Issue Created</h1>
-    <p>A new issue has been reported. ${issue.issuePDF} ${issue.issueCoverPhoto}</p>
-    <img src="${issue.issueCoverPhoto}" alt="Issue Cover" style="max-width: 100%; height: auto;" />
+    <p>A new issue has been reported. </p>
+    <img src={${issue.issueCoverPhoto}} alt="Issue Cover" style="max-width: 100%; height: auto;" />
     <p>
         <a href={${issue.issuePDF}} style="display: inline-block; padding: 10px 15px; background-color: #91373e; color: white; text-decoration: none; border-radius: 5px;">
-		{issue.issuePDF}
-		</a>
-        <a href="${issue.issuePDF}" style="display: inline-block; padding: 10px 15px; background-color: #91373e; color: white; text-decoration: none; border-radius: 5px;">
-		{issue.issueCoverPhoto}
+		View PDF
 		</a>
     </p>
   `;
