89b4c2fb5808e1b9186b26d721c596d4bf1b2018
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-10-31 08:02:25 +0530

    Update DashboardPage component to include an iframe with MongoDB Charts URL

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,7 +1,21 @@
 interface DashboardPageProps {}
 
 const DashboardPage = async ({}: DashboardPageProps) => {
-	return <div>Dashboard Page</div>;
+	return (
+		<div>
+			<iframe
+				style={{
+					background: "#F1F5F4",
+					border: "none",
+					borderRadius: "2px",
+					boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)",
+					width: "75vw",
+					height: "100vh",
+				}}
+				src={process.env.MONGODB_CHARTS_URL}
+			/>
+		</div>
+	);
 };
 
 export default DashboardPage;
