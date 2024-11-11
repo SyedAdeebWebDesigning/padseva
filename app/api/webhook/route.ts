239c3f3692d5930ee42f76044238358341880e7f
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-11-11 14:22:02 +0530

    Updated PDF Format

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,134 +1,134 @@
 import {
-	CreateUserProps,
-	updateUser,
-	createUser,
+  createUser,
+  CreateUserProps,
+  updateUser,
 } from "../../../lib/actions/User.action";
 import { Webhook } from "svix";
 import { headers } from "next/headers";
-import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
+import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-	const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
-	if (!WEBHOOK_SECRET) {
-		throw new Error(
-			"Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
-		);
-	}
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
+    );
+  }
 
-	const headerPayload = headers();
-	const svix_id = headerPayload.get("svix-id");
-	const svix_timestamp = headerPayload.get("svix-timestamp");
-	const svix_signature = headerPayload.get("svix-signature");
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
 
-	if (!svix_id || !svix_timestamp || !svix_signature) {
-		return new Response("Error occurred -- no svix headers", { status: 400 });
-	}
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error occurred -- no svix headers", { status: 400 });
+  }
 
-	const payload = await req.json();
-	const body = JSON.stringify(payload);
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
 
-	const wh = new Webhook(WEBHOOK_SECRET);
+  const wh = new Webhook(WEBHOOK_SECRET);
 
-	let evt: WebhookEvent;
+  let evt: WebhookEvent;
 
-	try {
-		evt = wh.verify(body, {
-			"svix-id": svix_id,
-			"svix-timestamp": svix_timestamp,
-			"svix-signature": svix_signature,
-		}) as WebhookEvent;
-	} catch (err) {
-		return new Response("Error occurred", { status: 400 });
-	}
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    return new Response("Error occurred", { status: 400 });
+  }
 
-	// Handle the user.created event
-	if (evt.type === "user.created") {
-		const {
-			id,
-			email_addresses,
-			image_url,
-			first_name,
-			last_name,
-			phone_numbers,
-		} = evt.data;
+  // Handle the user.created event
+  if (evt.type === "user.created") {
+    const {
+      id,
+      email_addresses,
+      image_url,
+      first_name,
+      last_name,
+      phone_numbers,
+    } = evt.data;
 
-		if (!email_addresses || email_addresses.length === 0) {
-			return new Response("Missing email address", { status: 400 });
-		}
+    if (!email_addresses || email_addresses.length === 0) {
+      return new Response("Missing email address", { status: 400 });
+    }
 
-		if (!phone_numbers || phone_numbers.length === 0) {
-			return new Response("Missing phone number", { status: 400 });
-		}
+    if (!phone_numbers || phone_numbers.length === 0) {
+      return new Response("Missing phone number", { status: 400 });
+    }
 
-		const user: CreateUserProps = {
-			clerkId: id,
-			email: email_addresses[0].email_address,
-			phone: phone_numbers[0].phone_number,
-			firstName: first_name ?? "",
-			lastName: last_name ?? "",
-			photo: image_url || "",
-			role: "Volunteer", // Default role can be changed if needed
-		};
+    const user: CreateUserProps = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      phone: phone_numbers[0].phone_number,
+      firstName: first_name ?? "",
+      lastName: last_name ?? "",
+      photo: image_url || "",
+      role: "Volunteer", // Default role can be changed if needed
+    };
 
-		try {
-			const newUser = await createUser(user);
+    try {
+      const newUser = await createUser(user);
 
-			// After user creation, update the Clerk metadata
-			if (newUser) {
-				await clerkClient.users.updateUserMetadata(id, {
-					publicMetadata: {
-						userId: newUser._id,
-					},
-				});
-			}
+      // After user creation, update the Clerk metadata
+      if (newUser) {
+        await clerkClient.users.updateUserMetadata(id, {
+          publicMetadata: {
+            userId: newUser._id,
+          },
+        });
+      }
 
-			return NextResponse.json({ message: "User created", user: newUser });
-		} catch (error: any) {
-			return new Response(`Error creating user: ${error.message}`, {
-				status: 500,
-			});
-		}
-	}
+      return NextResponse.json({ message: "User created", user: newUser });
+    } catch (error: any) {
+      return new Response(`Error creating user: ${error.message}`, {
+        status: 500,
+      });
+    }
+  }
 
-	// Handle the user.updated event
-	if (evt.type === "user.updated") {
-		const {
-			id,
-			email_addresses,
-			image_url,
-			first_name,
-			last_name,
-			phone_numbers,
-		} = evt.data;
+  // Handle the user.updated event
+  if (evt.type === "user.updated") {
+    const {
+      id,
+      email_addresses,
+      image_url,
+      first_name,
+      last_name,
+      phone_numbers,
+    } = evt.data;
 
-		if (!email_addresses || email_addresses.length === 0) {
-			return new Response("Missing email address", { status: 400 });
-		}
+    if (!email_addresses || email_addresses.length === 0) {
+      return new Response("Missing email address", { status: 400 });
+    }
 
-		if (!phone_numbers || phone_numbers.length === 0) {
-			return new Response("Missing phone number", { status: 400 });
-		}
+    if (!phone_numbers || phone_numbers.length === 0) {
+      return new Response("Missing phone number", { status: 400 });
+    }
 
-		const updateProps = {
-			email: email_addresses[0].email_address,
-			phone: phone_numbers[0].phone_number,
-			firstName: first_name ?? "",
-			lastName: last_name ?? "",
-			photo: image_url || "",
-		};
+    const updateProps = {
+      email: email_addresses[0].email_address,
+      phone: phone_numbers[0].phone_number,
+      firstName: first_name ?? "",
+      lastName: last_name ?? "",
+      photo: image_url || "",
+    };
 
-		try {
-			const updatedUser = await updateUser(id, updateProps);
-			return NextResponse.json({ message: "User updated", user: updatedUser });
-		} catch (error: any) {
-			return new Response(`Error updating user: ${error.message}`, {
-				status: 500,
-			});
-		}
-	}
+    try {
+      const updatedUser = await updateUser(id, updateProps);
+      return NextResponse.json({ message: "User updated", user: updatedUser });
+    } catch (error: any) {
+      return new Response(`Error updating user: ${error.message}`, {
+        status: 500,
+      });
+    }
+  }
 
-	// Return a default response for unknown event types
-	return new Response("Event type not handled", { status: 200 });
+  // Return a default response for unknown event types
+  return new Response("Event type not handled", { status: 200 });
 }
--- components/shared/NewsletterForm.tsx
+++ components/shared/NewsletterForm.tsx
@@ -1,177 +1,177 @@
 "use client";
 
-import {zodResolver} from "@hookform/resolvers/zod";
-import {useForm} from "react-hook-form";
-import {Form, FormControl, FormItem, FormMessage} from "@/components/ui/form";
-import {z} from "zod";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
-import React, {useState} from "react";
-import {toast} from "react-toastify";
-import {createNewsLetter, updateNewsLetter,} from "@/lib/actions/Newsletter.action";
-import {useRouter} from "next/navigation";
-import {INewsLetter} from "@/lib/database/model/Newsletter.model";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormItem, FormMessage } from "@/components/ui/form";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import {
+  createNewsLetter,
+  updateNewsLetter,
+} from "@/lib/actions/Newsletter.action";
+import { useRouter } from "next/navigation";
+import { INewsLetter } from "@/lib/database/model/Newsletter.model";
 
+// Define form schema
 const formSchema = z.object({
-    coverPhoto: z.object({
-        url: z.string().min(1, "Please upload a valid image file."),
-    }),
-    pdf: z.object({
-        url: z.string().min(1, "Please upload a valid PDF file."),
-    }),
+  coverPhoto: z.object({
+    url: z.string().min(1, "Please upload a valid image file."),
+  }),
+  pdf: z.object({
+    url: z.string().min(1, "Please enter a valid PDF URL."),
+  }),
 });
 
 const NewsletterForm = ({
-                            userClerkId,
-                            type,
-                            data,
-                        }:
-                            {
-                                userClerkId: string;
-                                type: "Create" | "Update";
-                                data?: INewsLetter;
-                            }
-    ) => {
-        const [imagePreview, setImagePreview] = useState<string | any>(
-            data?.issueCoverPhoto
-        );
-        const [pdfPreview, setPdfPreview] = useState<string | any>(data?.issuePDF);
-        const [isLoading, setIsLoading] = useState<boolean>(false)
-        const router = useRouter();
+  userClerkId,
+  type,
+  data,
+}: {
+  userClerkId: string;
+  type: "Create" | "Update";
+  data?: INewsLetter;
+}) => {
+  const [imagePreview, setImagePreview] = useState<string | any>(
+    data?.issueCoverPhoto,
+  );
+  const [pdfUrl, setPdfUrl] = useState<string | any>(data?.issuePDF);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
 
-        const form = useForm<z.infer<typeof formSchema>>({
-            resolver: zodResolver(formSchema),
-            defaultValues: {
-                coverPhoto: {url: data?.issueCoverPhoto || ""},
-                pdf: {url: data?.issuePDF || ""},
-            },
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      coverPhoto: { url: data?.issueCoverPhoto || "" },
+      pdf: { url: data?.issuePDF || "" },
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const newsletter = {
+      issueCoverPhoto: values.coverPhoto.url,
+      issuePDF: values.pdf.url,
+      userClerkId,
+    };
+
+    try {
+      setIsLoading(true);
+      if (type === "Create") {
+        await createNewsLetter({
+          ...newsletter,
         });
+        toast.success("Newsletter created successfully");
+      } else if (type === "Update" && data?._id) {
+        await updateNewsLetter(data._id, newsletter);
+        toast.success("Newsletter updated successfully");
+      }
 
-        async function onSubmit(values: z.infer<typeof formSchema>) {
-            const newsletter = {
-                issueCoverPhoto: values.coverPhoto.url,
-                issuePDF: values.pdf.url,
-                userClerkId,
-            };
-
-            try {
-                setIsLoading(true);
-                if (type === "Create") {
-                    await createNewsLetter({
-                        ...newsletter,
-                    });
-                    toast.success("Newsletter created successfully");
-                } else if (type === "Update" && data?._id) {
-                    await updateNewsLetter(data._id, newsletter);
-                    toast.success("Newsletter updated successfully");
-                }
-
-                setTimeout(() => {
-                    router.push("/newsletter");
-                    form.reset();
-                    setImagePreview(null);
-                    setPdfPreview(null);
-                }, 1500);
-            } catch (error) {
-                toast.error("Error submitting newsletter");
-            }
-        }
-
-        const handleFileChange = (
-            e: React.ChangeEvent<HTMLInputElement>,
-            type: "image" | "pdf"
-        ) => {
-            const files = e.target.files;
-            if (files) {
-                const file = files[0];
-                const reader = new FileReader();
-
-                reader.onload = (event: ProgressEvent<FileReader>) => {
-                    if (event.target && typeof event.target.result === "string") {
-                        if (type === "image") {
-                            setImagePreview(event.target.result);
-                            form.setValue("coverPhoto.url", event.target.result);
-                        } else {
-                            const pdfUrl = URL.createObjectURL(file);
-                            setPdfPreview(pdfUrl);
-                            form.setValue("pdf.url", pdfUrl);
-                        }
-                    }
-                };
-
-                reader.readAsDataURL(file);
-            }
-        };
-
-        return (
-            <div className="flex items-center justify-center min-h-screen">
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(onSubmit)}
-                        className="space-y-4 md:w-1/3 w-full bg-white shadow-lg p-6 rounded-md">
-                        <FormItem>
-                            <label htmlFor="coverPhoto" className="block text-gray-700">
-                                Cover Photo Upload
-                            </label>
-                            <FormControl>
-                                <Input
-                                    id="coverPhoto"
-                                    type="file"
-                                    accept="image/*"
-                                    onChange={(e) => handleFileChange(e, "image")}
-                                    className="mt-2"
-                                />
-                            </FormControl>
-                            {imagePreview && (
-                                <img
-                                    src={imagePreview}
-                                    alt="Cover Preview"
-                                    className="mt-2 w-full"
-                                />
-                            )}
-                            <FormMessage/>
-                        </FormItem>
-
-                        <FormItem>
-                            <label htmlFor="pdf" className="block text-gray-700">
-                                PDF Upload
-                            </label>
-                            <FormControl>
-                                <Input
-                                    id="pdf"
-                                    type="file"
-                                    accept="application/pdf"
-                                    onChange={(e) => handleFileChange(e, "pdf")}
-                                    className="mt-2"
-                                />
-                            </FormControl>
-                            {pdfPreview && (
-                                <a
-                                    href={pdfPreview}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="mt-2 text-blue-500">
-                                    View Uploaded PDF
-                                </a>
-                            )}
-                            <FormMessage/>
-                        </FormItem>
-
-                        <Button
-                            type="submit"
-                            variant={"padseva"}
-                            className="w-full bg-[#91373E]"
-                            disabled={!imagePreview || !pdfPreview || isLoading}
-                        >
-                            {type === "Create" ? `${isLoading ? "Creating..." : "Create Newsletter"}` : `${isLoading ? "Updating..." : "Update Newsletter"}`}
-                            <div
-                                className="absolute ease-[cubic-bezier(0.19,1,0.22,1)] -left-[75px] -top-[50px] -z-10 h-[155px] w-8 rotate-[35deg] bg-white/20 transition-all duration-500 group-hover:left-[120%]"/>
-                        </Button>
-                    </form>
-                </Form>
-            </div>
-        );
+      setTimeout(() => {
+        router.push("/newsletter");
+        form.reset();
+        setImagePreview(null);
+        setPdfUrl(null);
+      }, 1500);
+    } catch (error) {
+      toast.error("Error submitting newsletter");
     }
-;
+  }
+
+  const handleFileChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    type: "image" | "pdf",
+  ) => {
+    const files = e.target.files;
+    if (files) {
+      const file = files[0];
+      const reader = new FileReader();
+
+      reader.onload = (event: ProgressEvent<FileReader>) => {
+        if (event.target && typeof event.target.result === "string") {
+          if (type === "image") {
+            setImagePreview(event.target.result);
+            form.setValue("coverPhoto.url", event.target.result);
+          }
+        }
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handlePdfUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const url = e.target.value;
+    setPdfUrl(url);
+    form.setValue("pdf.url", url);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 md:w-1/3 w-full bg-white shadow-lg p-6 rounded-md"
+        >
+          {/* Cover Photo Upload */}
+          <FormItem>
+            <label htmlFor="coverPhoto" className="block text-gray-700">
+              Cover Photo Upload
+            </label>
+            <FormControl>
+              <Input
+                id="coverPhoto"
+                type="file"
+                accept="image/*"
+                onChange={(e) => handleFileChange(e, "image")}
+                className="mt-2"
+              />
+            </FormControl>
+            {imagePreview && (
+              <img
+                src={imagePreview}
+                alt="Cover Preview"
+                className="mt-2 w-full"
+              />
+            )}
+            <FormMessage />
+          </FormItem>
+
+          {/* PDF URL input */}
+          <FormItem>
+            <label htmlFor="pdf" className="block text-gray-700">
+              PDF URL
+            </label>
+            <FormControl>
+              <Input
+                id="pdf"
+                type="text"
+                value={pdfUrl || ""}
+                onChange={handlePdfUrlChange}
+                className="mt-2"
+                placeholder="Enter the PDF URL"
+              />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+
+          {/* Submit Button */}
+          <Button
+            type="submit"
+            variant={"padseva"}
+            className="w-full bg-[#91373E]"
+            disabled={!imagePreview || !pdfUrl || isLoading}
+          >
+            {type === "Create"
+              ? `${isLoading ? "Creating..." : "Create Newsletter"}`
+              : `${isLoading ? "Updating..." : "Update Newsletter"}`}
+            <div className="absolute ease-[cubic-bezier(0.19,1,0.22,1)] -left-[75px] -top-[50px] -z-10 h-[155px] w-8 rotate-[35deg] bg-white/20 transition-all duration-500 group-hover:left-[120%]" />
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
 
 export default NewsletterForm;
