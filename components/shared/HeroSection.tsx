7737bf116e3439264b56a9e98d1e16a27a6f41ec
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2025-01-01 05:09:43 +0530

    Updated Images.

--- components/shared/HeroSection.tsx
+++ components/shared/HeroSection.tsx
@@ -5,24 +5,26 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const HeroSection: React.FC = () => {
-	return (
-		<div className="sticky z-50 top-0 w-full h-screen bg-[#F7E7EF] overflow-hidden flex items-center justify-center">
-			<div className="relative text-center">
-				<motion.div
-					className="relative lg:size-[600px] size-[400px]" // Ensure the parent div has a height and width
-					initial={{ scale: 0 }}
-					whileInView={{ scale: 1 }}
-					transition={{ duration: 0.8, ease: "easeOut" }}>
-					<Image
-						src="/padseva-logo-2.png"
-						alt="logo"
-						fill
-						style={{ objectFit: "contain" }}
-					/>
-				</motion.div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="sticky z-50 top-0 w-full h-screen bg-[#F7E7EF] overflow-hidden flex items-center justify-center">
+      <div className="relative text-center">
+        <motion.div
+          className="relative lg:size-[600px] size-[400px]" // Ensure the parent div has a height and width
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          <Image
+            src="/padseva-logo-2.webp"
+            alt="logo"
+            loading={"eager"}
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
