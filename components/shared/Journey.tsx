087d41b6314b4e0e89e8963bd2a5acf86a1d4acd
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2025-01-01 05:15:03 +0530

    Updated Images.

--- components/shared/Journey.tsx
+++ components/shared/Journey.tsx
@@ -6,8 +6,6 @@ import NumberTicker from "@/components/ui/number-ticker";
 interface JourneyProps {}
 
 const Journey = ({}: JourneyProps) => {
-  // Define initial state to hold the animated values
-
   const JourneyItems = [
     {
       title: "Bamboo Pads Synthesized",
@@ -15,7 +13,7 @@ const Journey = ({}: JourneyProps) => {
       isMore: false,
     },
     {
-      title: "Womans Met",
+      title: "Women Met",
       value: 9000,
       isMore: true,
     },
@@ -30,6 +28,7 @@ const Journey = ({}: JourneyProps) => {
       isMore: true,
     },
   ];
+
   return (
     <div className="mx-auto flex w-full flex-col justify-center items-center sm:items-start p-10">
       <picture className="relative w-[250px] md:w-[300px] lg:w-[450px] lg:h-20 h-[50px] font-semibold z-[100] ">
@@ -41,18 +40,25 @@ const Journey = ({}: JourneyProps) => {
           className="lg:ml-[255px] object-left"
         />
       </picture>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4  text-white w-full py-10 container mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 text-white w-full py-10 container mx-auto">
         {JourneyItems.map((item, index) => (
           <div
             key={index}
             className="flex flex-col items-center justify-center p-4 times-new-roman"
           >
-            <p className="whitespace-pre-wrap text-6xl font-medium text-gray-200">
-              <NumberTicker value={item.value} className={"text-gray-200"} />
+            <p
+              className="whitespace-pre-wrap text-6xl font-medium"
+              style={{ color: "#ffffff", textShadow: "1px 1px 5px #000000" }}
+            >
+              <NumberTicker value={item.value} className={"text-white"} />
               {item.isMore && <span>+</span>}
             </p>
-
-            <p className="text-xl">{item.title}</p>
+            <p
+              className="text-xl"
+              style={{ color: "#ffffff", textShadow: "1px 1px 5px #000000" }} // Ensure sufficient contrast
+            >
+              {item.title}
+            </p>
           </div>
         ))}
       </div>
