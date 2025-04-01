390bb84f887361688ee7384caf73fae02bd6597c
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2025-04-01 09:34:17 +0530

    Refactor function to improve performance

--- components/shared/NewsLetter.tsx
+++ components/shared/NewsLetter.tsx
@@ -7,17 +7,20 @@ interface NewsLetterProps {}
 // Array of issues with their respective links
 const issues = [
 	{
-		title: "Issue 1",
-		url: "https://www.lablinks.co.in/_files/ugd/bf27d6_ed1eb3d22728497495e9a7c96a931a6d.pdf",
+		title: "Newsletter 1",
+		url: "https://drive.google.com/file/d/1DX6iawqk9fXfe7bgSvg9fV8VtgH9iszA/view?usp=sharing",
+		imgUrl: "/assets/Newsletter-1.png",
 	},
-	{
-		title: "Issue 2",
-		url: "https://www.lablinks.co.in/_files/ugd/bf27d6_ed1eb3d22728497495e9a7c96a931a6d.pdf",
-	},
-	{
-		title: "Issue 3",
-		url: "https://www.lablinks.co.in/_files/ugd/bf27d6_ed1eb3d22728497495e9a7c96a931a6d.pdf",
-	},
+	// {
+	// 	title: "Issue 2",
+	// 	url: "https://www.lablinks.co.in/_files/ugd/bf27d6_ed1eb3d22728497495e9a7c96a931a6d.pdf",
+	// 	imgUrl: "/assets/Newsletter-1.png",
+	// },
+	// {
+	// 	title: "Issue 3",
+	// 	url: "https://www.lablinks.co.in/_files/ugd/bf27d6_ed1eb3d22728497495e9a7c96a931a6d.pdf",
+	// 	imgUrl: "/assets/Newsletter-1.png",
+	// },
 ];
 
 const NewsLetter = ({}: NewsLetterProps) => {
@@ -49,9 +52,19 @@ const NewsLetter = ({}: NewsLetterProps) => {
 				{/* Dynamically rendering newsletter issues */}
 				<div className="w-full max-w-7xl mx-auto my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 md:gap-8">
 					{issues.map((issue, index) => (
-						<Link href={issue.url} target="_blank" key={index} className="">
-							<div className="w-full h-[500px] md:h-[600px] bg-gray-100 flex items-center justify-center text-2xl sm:text-3xl">
-								{issue.title}
+						<Link
+							href={issue.url}
+							target="_blank"
+							key={index}
+							className="block">
+							<div className="w-full h-[500px] md:h-[600px] flex flex-col items-center justify-center text-2xl sm:text-3xl rounded-lg shadow-lg overflow-hidden">
+								<Image
+									src={issue.imgUrl}
+									alt={issue.title}
+									width={500} // Adjust as needed
+									height={600} // Adjust as needed
+									className="w-full h-full object-cover"
+								/>
 							</div>
 						</Link>
 					))}
